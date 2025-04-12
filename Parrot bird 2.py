7e644eaede492e9d0c53f7711d5e264f1c5af439
Anugraha,20git config --global username <champchandanrout@gmail.com>
2025-04-12 19:37:29 +0530

    Parrot 2

---
+++ Parrot bird 2.py
@@ -0,0 +1,25 @@
+class Parrot
+
+
+  # instace attributes
+  # def _init_(self, name, age):
+  self.name = name
+  self.age = age
+
+
+# instance method
+def sing(self, song):
+  return "{} sings {}".format(self,name, song)
+
+
+def dance(self)
+  return "{} is now dancing".format(self.name)
+
+
+  instantiate the object
+  blu = Parrot("Blu", 10)
+
+
+  #call our instance methods
+  print(blu.sing("'Happy"))
+  print(blu.dance())

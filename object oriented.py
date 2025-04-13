d1a9a059d2dddc324ebd7ea95a1ad75974b34df8
Anugraha,20git config --global username <champchandanrout@gmail.com>
2025-04-13 19:08:31 +0530

    Object oriented python

---
+++ object oriented.py
@@ -0,0 +1,12 @@
+class Rectangle():
+    def _init_(self, 1, w):
+        self.length = 1
+        self.width = while
+
+        def rectagle_area(self):
+            return self.length*self.width
+        
+
+newRectangle = Rectangle(12, 10)
+print("Dimention of Rectagle - Length : %d width : %d" % (newRectengle.Length, newRectangle.width))
+print("Area of Rectangle :", newRectangle.rectangle_area())

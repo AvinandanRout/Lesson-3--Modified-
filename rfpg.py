f948679aa39d524ac3e623c3f8a94c08f2ae4cf6
Anugraha,20git config --global username <champchandanrout@gmail.com>
2025-04-19 19:55:04 +0530

    rfpg.py

---
+++ rfpg.py
@@ -0,0 +1,36 @@
+import random
+fname = ['John', 'Jane', 'Corey', ' Travis', 'Dave']
+ 
+iname = ['Smith','Doe','Jenkins','Robinson','Davis']
+
+street_names = ['Main','High','Pearl','Maple','Park']
+
+states = ['AL','AK,'AZ','AR', 'CA']
+          
+class Name:
+   def _init_(self,num=1)
+
+   self.num = num
+
+   def first_name(self):
+   #print first name
+   for i in range(self.num):
+   first = random.choice(fname)
+   print(first)
+
+
+   def last_nae(self):
+   #print last name
+   for i in range(seld.num):
+   last = random.choice(iname)
+   print(last)
+
+
+   def full name
+   for i in range(self.num):
+   first = random.choice(fname)
+   last = random.choice(iname)
+   phone = f'+91-(random.randint(800, 999)){random.radint(1000, 9999)}
+
+
+   

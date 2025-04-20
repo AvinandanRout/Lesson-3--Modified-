a242f271697c6ea7903eda07a4dacc5d9b964ca5
Anugraha,20git config --global username <champchandanrout@gmail.com>
2025-04-20 19:51:01 +0530

    Natural number

---
+++ natural number.py
@@ -0,0 +1,12 @@
+number = int(imput("enter number to check"))
+print("Number to be checked :", number)
+
+
+if number>0:
+    print("This is a Natural number")
+
+elif number<0;
+    print("Ths is a innatural number")
+
+else:
+    print("This is neutral")        
